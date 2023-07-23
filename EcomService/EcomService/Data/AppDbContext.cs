33e3042219aea273b1daa87468fd7d82872531f9
Pathum99 <pathumnakandala@gmail.com>
2023-07-23 18:20:06 +0530

    DB Configurations

---
+++ EcomService/EcomService/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EcomService.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcomService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+
+    }
+}
